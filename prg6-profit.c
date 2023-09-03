54cc502296314fc02cf2f50c6e1303a1e3289bb6
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-03 06:26:43 -0700

    prg6 added sucessfully

---
+++ prg6-profit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int total;
+	float sell,profit,cost;
+	printf("enter the total items:");
+	scanf("%d",&total);
+	printf("\n enter the selling price of %d items:");
+	scanf("%f",&sell);
+	printf("\n enter the profit:");
+	scanf("%f",&profit);
+	cost=(sell-profit)/total;
+	printf("\n cost of each item is RS.%f",cost);
+	return 1;
+}
