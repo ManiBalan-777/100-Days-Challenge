cd201129adf7132e7811018bb99660714f3a91ca
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-05 08:13:47 -0700

    added successfully

---
+++ prg8-switch.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int ch;
+	printf("\n enter the choice:");
+	scanf("%d",&ch);
+	switch(ch)
+	{
+		case 1:
+			printf("\n one");
+			break;
+		case 2:
+			printf("\n two");
+			break;
+		case 3:
+			printf("\n three");
+			break;
+			
+	}
+	return 1;
+	
+}
