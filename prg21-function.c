ae3dd409bbcdb8338a251e5111e779b2f78a3a3f
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-07 08:59:36 -0700

    function basics'

---
+++ prg21-function.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void add()
+{
+		int a,b,c;
+	printf("\n enter a:");
+	scanf("%d",&a);
+	printf("\n enter b:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("\n total=%d",c);
+}
+int main()
+{
+//	add();
+//	add();
+//	add();
+	int n,i;
+	printf("\n Enter the limit:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		add();
+	}
+		
+	return 0;
+}
