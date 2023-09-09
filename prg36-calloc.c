a938571db6e41ce7ef0dad8874c527167154148b
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-09 03:55:40 -0700

    calloc

--- prg36-calloc.c
+++ prg36-calloc.c
@@ -5,7 +5,7 @@ int main()
 	int i,n;
 	printf("\n Enter the limit:");
 	scanf("%d",&n);
-	int *p=(int *)malloc(sizeof(int)*n);
+	int *p=(int *)calloc(n,sizeof(int));
 	if(p==NULL)
 	{
 		printf("\n Memory not allocated");
@@ -16,7 +16,14 @@ int main()
 		printf("\nRandom value=%d	",*(p+i));
 		printf("\n Enter the number that is New value assigned to Random value=:");
 		scanf("%d",(p+i));
-		printf("Print the real value=%d	",*(p+i));
+		printf("\nPrint the address value=%d	",&p+i);
+		printf("\nPrint the real value=%d	",*(p+i));
+		printf("\n");
+	}
+	for(i=0;i<n;i++)
+	{
+		
+		printf("Address=%d :Value=%d	",&p+i,*(p+i));
 		printf("\n");
 	}
 }
