19027ffa04f861e15dc0d5f9aa0db058068d503d
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-09 03:35:12 -0700

    malloc

---
+++ prg35-malloc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i,n;
+	printf("\n Enter the limit:");
+	scanf("%d",&n);
+	int *p=(int *)malloc(sizeof(int)*n);
+	if(p==NULL)
+	{
+		printf("\n Memory not allocated");
+		exit(1);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("\nRandom value=%d	",(p+i));
+		printf("\n Enter the number that is New value assigned to Random value=:");
+		scanf("%d",(p+i));
+		printf("Print the real value=%d	",*(p+i));
+		printf("\n");
+	}
+}
